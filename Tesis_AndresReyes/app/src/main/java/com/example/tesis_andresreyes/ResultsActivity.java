6b6dd5c4f5ce6f550308905fef00aee5829ed7ee
Andres Reyes <andresreyesf@usantotomas.edu.co>
2020-06-05 18:18:31 -0500

    SpeechProblem fixed

--- Tesis_AndresReyes/app/src/main/java/com/example/tesis_andresreyes/ResultsActivity.java
+++ Tesis_AndresReyes/app/src/main/java/com/example/tesis_andresreyes/ResultsActivity.java
@@ -1,9 +1,12 @@
 package com.example.tesis_andresreyes;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,6 +15,7 @@ import java.util.Locale;
 public class ResultsActivity extends AppCompatActivity {
 
     private TextView wordText;
+    private boolean flagSpeak;
 
     private TextToSpeech speechMachine;
     private String finalWord;
@@ -21,8 +25,8 @@ public class ResultsActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
-
         wordText = findViewById(R.id.t_word);
+        flagSpeak = true;
         ////Receive message by launcher activity(Main)
         Intent i = getIntent();
         finalWord = i.getStringExtra("speechText");
@@ -33,18 +37,36 @@ public class ResultsActivity extends AppCompatActivity {
             @Override
             public void onInit(int status)
             {
-                if(status != TextToSpeech.ERROR)
-                {
-                    Locale locSpanish = new Locale("spa", "COL");
-                    speechMachine.setLanguage(locSpanish);
-                }
+                Locale locSpanish = new Locale("spa", "COL");
+                speechMachine.setLanguage(locSpanish);
                 if (status == TextToSpeech.SUCCESS)
                 {
                     ///here the machine can start to speak
                     speechMachine.speak(finalWord,TextToSpeech.QUEUE_ADD, null, null);
                 }
+                else
+                {
+                    Handler speakHandler = new Handler();
+                    speakHandler.postDelayed(new Runnable() {
+                        @Override
+                        public void run()
+                        {
+                            speechMachine.speak(finalWord,TextToSpeech.QUEUE_ADD, null, null);
+                        }
+                    },2000);
+                }
             }
         });
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        if(flagSpeak)
+        {
+            speechMachine.speak(finalWord,TextToSpeech.QUEUE_ADD, null, null);
+            flagSpeak = false;
+        }
+        return true;
     }
 
     public void exitApp(View view)
